8d02b2b42702fa4c038330b8c8d5513f726bce2f
Jenny Massari <jennymassari@ookla-mac-jennymassari.local>
2025-04-28 13:56:45 -0700

    Retrieved test summary metadata by collection set id.

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
 import pandas as pd
 from dotenv import load_dotenv
 import os
+import time
 
 load_dotenv()
 
 
-df_metadata = f'''select
+csid = 12736
+
+df_metadata = f'''
+SELECT
    date_trunc('seconds',ts.start_time)::timestamp without time zone as start_time,
    ca.time_zone,
    vcs.collection_type,
    vcs.collection_set,
    ts.collection_set_id,
    ts.test_type_id,
-   c.friendly_name as carrier,
+   c.friendly_name AS carrier,
    ts.net_types,
    ts.call_network_type, -- Note: Substitute for `co_call_network_type`
    ts.flag_access_success,
@@ -29,22 +33,26 @@ df_metadata = f'''select
                                           tea.nr_status_filtered, 
                                           tea.nr_bearer_status_filtered,
                                           tea.nr_bearer_allocation_status_filtered,
-                                          ','
-                                          ) best_network_type,
+                                          ',') AS best_network_type,
    md2.fn_get_network_types(ts.net_types, ','::character varying) AS net_types_desc,
    kt.name AS kit_type,
-   rsc.name as report_set_collector,
-  (start_time at time zone 'utc' at time zone ca.time_zone) as local_time
-from prod_ms_partitions.test_summary_?curr_csid ts
-left join md2.collection_areas ca on (ts.collection_area_id = ca.collection_area_id)
-left join md2.vi_collection_sets vcs on (ts.collection_set_id = vcs.collection_set_id)
-left join md2.carriers c on (ts.carrier_id = c.carrier_id)
-left join prod_rsr_partitions.test_event_aggr_?curr_csid tea on (ts.collection_set_id = tea.collection_set_id and ts.test_event_id = tea.test_event_id)
-left join md2.kit_types kt on (kt.kit_type_id = ts.kit_type_id)
-left join md2.report_set_collectors rsc on (rsc.report_set_collector_id = ts.report_set_collector_id)
-where ts.test_type_id IN (19, 20, 23) and ts.collection_set_period_id IS NOT NULL and ts.blacklisted = FALSE and ts.flag_valid = TRUE and ts.carrier_id <> 478'''
+   rsc.name AS report_set_collector,
+   (ts.start_time AT TIME ZONE 'utc' AT TIME ZONE ca.time_zone) AS local_time
+FROM prod_ms_partitions.test_summary_{csid} as ts
+LEFT JOIN md2.collection_areas ca on (ts.collection_area_id = ca.collection_area_id)
+LEFT JOIN md2.vi_collection_sets vcs on (ts.collection_set_id = vcs.collection_set_id)
+LEFT JOIN md2.carriers c on (ts.carrier_id = c.carrier_id)
+LEFT JOIN prod_rsr_partitions.test_event_aggr_{csid} tea on (ts.collection_set_id = tea.collection_set_id and ts.test_event_id = tea.test_event_id)
+LEFT JOIN md2.kit_types kt on (kt.kit_type_id = ts.kit_type_id)
+LEFT JOIN md2.report_set_collectors rsc on (rsc.report_set_collector_id = ts.report_set_collector_id)
+WHERE ts.test_type_id IN (19, 20, 23) 
+AND ts.collection_set_period_id IS NOT NULL 
+AND ts.blacklisted = FALSE 
+AND ts.flag_valid = TRUE 
+AND ts.carrier_id <> 478
+'''
 
-df_metadata = pd.read_sql_query(df_metadata, con=os.getenv('DATABASE_URL'))
+df_test_summary_metadata_csid = pd.read_sql_query(df_metadata, con=os.getenv('RSR_SVC_CONN'))
+print(df_test_summary_metadata_csid)
 
-print(df_metadata)
 
